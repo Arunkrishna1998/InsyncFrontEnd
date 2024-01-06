f3733870931d17815a8c7352a76966ca3f4a64e0
Arunkrishna1998 <arunkvb98@gmail.com>
2024-01-06 21:18:42 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const currentProtocol = window.location.protocol;
+// const baseDomain = 'nextnode.jithinmanilal.site';
+// const BASE_URL = `${currentProtocol}//${baseDomain}`;
+
+// const BASE_URL = 'http://127.0.0.1:8000';
+
+const BASE_URL = 'http://13.49.44.20';
+
+
+
+export { BASE_URL };
