9fe9c30c86493cdd11519e26100e561d975e54e8
Arunkrishna1998 <arunkvb98@gmail.com>
2024-01-07 16:49:49 +0530

    Updated Config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const currentProtocol = window.location.protocol;
 // const BASE_URL = `${currentProtocol}//${baseDomain}`;
 
-// const BASE_URL = 'http://127.0.0.1:8000';
+const BASE_URL = 'https://www.arunkrishna.online';
 
-const BASE_URL = 'http://13.49.44.20';
+// const BASE_URL = 'http://13.49.44.20';
 
 
 
